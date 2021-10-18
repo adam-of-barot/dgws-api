97d7fcda3ef5a6083124e061353bc32cec134793
adam-of-barot <adam-of-barot@users.noreply.github.com>
2021-10-18 11:37:11 +0200

    Expand ServiceRequest test function to test more args

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -22,14 +22,32 @@ class Test(TestCase):
 
     def test_service_request_function(self):
         from dgws_api.functions import _service_request_dict
-        # default return should be a basic ServiceRequest object as a dict with page=1
-        self.assertEqual(_service_request_dict(), ServiceRequest(page=1).__dict__)
+        # default return should be a basic ServiceRequest object as a dict with page=1 and count 200
+        self.assertEqual(_service_request_dict(), ServiceRequest(page=1, count=200).__dict__)
+        # if 'request' argument is given, return that ServiceRequest object as a dict
+        self.assertEqual(_service_request_dict(ServiceRequest(page=2, count=100)), ServiceRequest(page=2, count=100).__dict__)
+
+        # ---- Test PAGE argument ----
+
         # if 'page' arg is given, return a default ServiceRequest object as a dict with appropriate page
         self.assertEqual(_service_request_dict(page=2), ServiceRequest(page=2).__dict__)
-        # if 'request' argument is given, return that ServiceRequest object as a dict
-        self.assertEqual(_service_request_dict(ServiceRequest(page=2)), ServiceRequest(page=2).__dict__)
         # if both arguments are given, return that ServiceRequest object as a dict with page adjusted
         self.assertEqual(_service_request_dict(ServiceRequest(page=2), page=3), ServiceRequest(page=3).__dict__)
+
+        # ---- Test PAGE_SIZE argument ----
+
+        # if 'page_size' arg is given, return a default ServiceRequest object as a dict with appropriate count
+        self.assertEqual(_service_request_dict(page_size=100), ServiceRequest(count=100).__dict__)
+        # if both arguments are given, return that ServiceRequest object as a dict with count adjusted
+        self.assertEqual(_service_request_dict(ServiceRequest(count=100), page_size=300), ServiceRequest(count=300).__dict__)
+
+        # ---- Test PAGE and PAGE_SIZE arguments together ----
+
+        # if both page and page_size argument is given, return a ServiceRequest object as a dict with both set
+        self.assertEqual(_service_request_dict(page=2, page_size=100), ServiceRequest(page=2, count=100).__dict__)
+        # if 'request', 'page' and 'page_size' arguments are given,
+        # return that ServiceRequest object as a dict with page and count adjusted
+        self.assertEqual(_service_request_dict(ServiceRequest(page=2, count=100), page=3, page_size=1000), ServiceRequest(page=3, count=1000).__dict__)
 
     def test_search_name(self):
         try:
